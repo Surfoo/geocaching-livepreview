1b4ae2ed60da1428b791f249319dd7d090451313
Surfoo <surfooo@gmail.com>
2022-06-09 00:17:46 +0200

    removed footer, added modal

--- public/app.js
+++ public/app.js
@@ -98,3 +98,19 @@ const copyToClipboard = () => {
     document.getElementById("copy").innerHTML = "📋 Copy";
   }, 1000);
 };
+
+const modals = document.querySelectorAll("[data-modal]");
+modals.forEach(function (trigger) {
+  trigger.addEventListener("click", function (event) {
+    event.preventDefault();
+    const modal = document.getElementById(trigger.dataset.modal);
+    modal.classList.add("modal-open");
+    const exits = modal.querySelectorAll(".modal-exit");
+    exits.forEach(function (exit) {
+      exit.addEventListener("click", function (event) {
+        event.preventDefault();
+        modal.classList.remove("modal-open");
+      });
+    });
+  });
+});
