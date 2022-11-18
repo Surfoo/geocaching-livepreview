a7732894af7558ea62d584fe30e935dd0fbbb072
Surfoo <surfooo@gmail.com>
2022-11-19 00:15:44 +0100

    Replaced rollup by esbuild

--- src/app.js
+++ src/app.js
@@ -30,7 +30,6 @@ document.addEventListener("DOMContentLoaded", () => {
     view = new EditorView({
         doc,
         extensions: [
-            basicSetup(),
             history(),
             keymap.of([...defaultKeymap, ...historyKeymap]),
             lineNumbers(),
@@ -122,15 +121,7 @@ const storageAvailable = (type) => {
         return (
             e instanceof DOMException &&
             // everything except Firefox
-            (e.code === 22 ||
-                // Firefox
-                e.code === 1014 ||
-                // test name field too, because code might not be present
-                // everything except Firefox
-                e.name === "QuotaExceededError" ||
-                // Firefox
-                e.name === "NS_ERROR_DOM_QUOTA_REACHED") &&
-            // acknowledge QuotaExceededError only if there's something already stored
+            (e.name === "QuotaExceededError" || e.name === "NS_ERROR_DOM_QUOTA_REACHED") &&
             storage &&
             storage.length !== 0
         )
